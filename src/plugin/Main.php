13cce577d9065900a5eb6d5f5403d5b36835a47e
ChaseDukeYT <81032681+ChaseDukeYT@users.noreply.github.com>
2021-04-21 13:00:49 -0400

    added a command

--- src/plugin/Main.php
+++ src/plugin/Main.php
@@ -4,11 +4,15 @@ namespace youtube;
 
 use pocketmine\plugin\PluginBase;
 
+use pocketmine\command\Command;
+      
+use pocketmine\command\CommandSender;
+
 class Main extends PluginBase
 {
     public function onLoad()
     {
-      
+        $sender->sendMessage("§4Youtube§r§1Plugin Ready");
     }
 
     public function onEnable()
@@ -20,4 +24,12 @@ class Main extends PluginBase
     {
         /** code */
     }
+    public function onCommand(CommandSender $sender, Command $cmd, string $label, array $args) : bool {
+  switch($cmd->getName()) { 
+    case "yt": 
+      $sender->sendMessage("1000 Subscribers Needed!"); 
+    break; 
+  }
+  return true;
+}
 }
